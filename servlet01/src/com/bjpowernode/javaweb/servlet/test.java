7b558912f323e6f63b5929e373bf013c206ef008
mj <1653487457@qq.com>
2022-04-19 09:58:24 +0800

    first commit

---
+++ servlet01/src/com/bjpowernode/javaweb/servlet/test.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.javaweb.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class test extends chouxiang {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println(this.confing);
+    }
+
+    @Override
+    public void init() {
+        System.out.println("巧妙地设计模式");
+    }
+}
